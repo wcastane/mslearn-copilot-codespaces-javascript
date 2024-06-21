20279a0182e9808b29d58835bbebabb1504fe730
William Castañeda <32845618+wcastane@users.noreply.github.com>
2024-06-21 02:31:21 +0000

    my changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "William Castañeda",
+  title: "Coder & IT Leader",
+  email: "william@example.com",
+  gitHub: "https://github.com/wcastane",
+  instagram: "wcastane",
+  linkedIn: "https://www.linkedin.com/in/wcastane/",
   medium: "",
-  twitter: "microsoft",
+  twitter: "https://x.com/william_abraham",
   youTube: "Code",
 };
 
